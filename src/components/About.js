ee2765364573400211dfe5378207bd773af28005
Mohcka <bandagedwolf95@gmail.com>
2019-10-17 14:38:13 -0700

    update about description

--- src/components/About.js
+++ src/components/About.js
@@ -18,9 +18,17 @@ const About = () => (
       <h4>
         I'm a jr web developer who has an affinity for javascript,{" "}
         <a href="https://reactjs.org/">React</a>, and{" "}
-        <a href="https://nodejs.org/en/about/">nodejs</a>
+        <a href="https://nodejs.org/en/about/">NodeJS</a>, and more...
       </h4>
-      <p>
+      <div>
+        <p>
+          I'm a young and motivated web devloper who loves to play around with
+          the latest and greatest programming languages (Java, javascript, php,
+          Ruby and Python) and technologies (SQL, AJAX, React, Git, CLIs) to
+          create astounding and performant web experiences. I've knowledge of
+          many of the best coding practices and tools creating RESTful web
+          applications from both the front end and back.
+        </p>
         <p>
           I started off learning Java in my highschool's AP Computer Science
           course. As I got to college I continued taking courses on other
@@ -29,11 +37,11 @@ const About = () => (
         </p>
         <p>
           I currently work as a web developer at my company, building engaging
-          web experiences with WordPress. I also occasionally write programs 
+          web experiences with WordPress. I also occasionally write programs
           that handle the data we use everyday that takes advantage of several
           APIs of the services we use.
         </p>
-      </p>
+      </div>
     </Container>
   </StyledAboutWrapper>
 )
