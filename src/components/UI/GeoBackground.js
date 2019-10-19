650cd1893610ea4a62916b2b9eba05f894e0bb53
Mohcka <bandagedwolf95@gmail.com>
2019-10-18 19:35:16 -0700

    added particle ackground

--- src/components/UI/GeoBackground.js
+++ src/components/UI/GeoBackground.js
@@ -3,8 +3,9 @@ import PropTypes from "prop-types"
 import GeoPattern from "geopattern"
 import theme from "../../theme"
 
-const GeoBackground = ({ children }) => (
+const GeoBackground = ({ children, className }) => (
   <div
+  className={className}
     style={{
       backgroundImage: GeoPattern.generate("ok", {
         generator: "xes",
@@ -18,6 +19,7 @@ const GeoBackground = ({ children }) => (
 
 GeoBackground.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 }
 
 export default GeoBackground
--- src/components/banner-v2.js
+++ src/components/banner-v2.js
@@ -1,18 +1,39 @@
 import React from "react"
 import Loadable from "react-loadable"
+import styled from "styled-components"
 
 const AnimTextList = Loadable({
   loader: () => import("./animation/AnimTextList"),
-  loading: () => <span>javascript</span>,
+  loading: () => <span style={{ color: "white", opacity: 0 }}>javascript</span>,
 })
 
-const GeoBackground = Loadable({
-  loader: () => import("./UI/GeoBackground"),
-  // eslint-disable-next-line react/prop-types
-  loading: ({ children }) => (
-    <div style={{ background: "red" }}>{children}</div>
-  ),
+// const GeoBackground = Loadable({
+//   loader: () => import("./UI/GeoBackground"),
+//   loading: () => (
+//     <div className="background"></div>
+//   ),
+// })
+
+const ParticlesBG = Loadable({
+  loader: () => import("./UI/PartclesBackground"),
+  loading: () => <div className="background"></div>
 })
+
+const StyledBanner = styled.div`
+  height: 400px;
+  color: white;
+  position: relative;
+
+  .background {
+    background-color: ${props => props.theme.primaryDark};
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: -10;
+  }
+`
 
 const Banner = () => {
   const techList = [
@@ -27,38 +48,37 @@ const Banner = () => {
   ]
 
   return (
-    <GeoBackground>
-      <div style={{ height: 400, color: "white" }}>
-        <div
-          className="intro"
+    <StyledBanner>
+      <ParticlesBG className="background" />
+      <div
+        className="intro"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+          padding: "20px",
+        }}
+      >
+        <h2
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            padding: "20px",
+            color: "white",
           }}
         >
-          <h2
-            style={{
-              color: "white",
-            }}
-          >
-            Hi, I'm Michael
-          </h2>
-          <h4
-            style={{
-              color: "white",
-            }}
-          >
-            I'm an inspired web developer creating{" "}
-            <AnimTextList textList={techList} /> applications
-          </h4>
-        </div>
+          Hi, I'm Michael
+        </h2>
+        <h4
+          style={{
+            color: "white",
+          }}
+        >
+          I'm an inspired web developer creating{" "}
+          <AnimTextList textList={techList} /> applications
+        </h4>
       </div>
-    </GeoBackground>
+    </StyledBanner>
   )
 }
 
